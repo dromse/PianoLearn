a085ddf51fc8633b2c30bd5f3c52372dc1ba3d51
dromse-laptop <dromasko4@gmail.com>
2021-01-01 23:21:42 +0200

    Update

--- src/pianolearn/controllers/PianoController.java
+++ src/pianolearn/controllers/PianoController.java
@@ -1,63 +1,42 @@
 package pianolearn.controllers;
 
-import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
-import pianolearn.classes.Note;
-
 import javax.sound.midi.MidiChannel;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.MidiUnavailableException;
 import javax.sound.midi.Synthesizer;
 import java.io.IOException;
 import java.net.URL;
-import java.util.Arrays;
-import java.util.List;
 import java.util.ResourceBundle;
 
 public class PianoController implements Initializable {
-
     private MidiChannel channel;
-
-    private final List<Note> notes = Arrays.asList(
-            new Note("C1", KeyCode.Q, 60),
-            new Note("CSharp1", KeyCode.W, 61),
-            new Note("D1", KeyCode.E, 62),
-            new Note("DSharp1", KeyCode.R, 63),
-            new Note("E1", KeyCode.T, 64),
-            new Note("F1", KeyCode.Y, 65),
-            new Note("FSharp1", KeyCode.U, 66),
-            new Note("G1", KeyCode.I, 67),
-            new Note("GSharp1", KeyCode.O, 68),
-            new Note("A1", KeyCode.P, 69),
-            new Note("ASharp1", KeyCode.OPEN_BRACKET, 70),
-            new Note("C2", KeyCode.CLOSE_BRACKET, 71)
-    );
-
-    /* load sound of synthesizer: Piano */
-    private void loadChannel() {
+    Synthesizer synth;
+    {
         try {
-            Synthesizer synth = MidiSystem.getSynthesizer();
-            synth.open();
-            synth.loadInstrument(synth.getDefaultSoundbank().getInstruments()[0]);
-
-            channel = synth.getChannels()[0];
-
+            synth = MidiSystem.getSynthesizer();
         } catch (MidiUnavailableException e) {
             System.out.println("Cannot get synth.");
             e.printStackTrace();
         }
     }
 
+    /* load sound of synthesizer: Piano */
+    private void loadChannel() throws MidiUnavailableException {
+            synth.open();
+            synth.loadInstrument(synth.getDefaultSoundbank().getInstruments()[0]);
+            channel = synth.getChannels()[0];
+    }
+
     // when this method is called -> change scene to main menu
     public void openMainMenu(ActionEvent actionEvent) {
         try {
+            synth.close();
             Stage window = (Stage)((Node) actionEvent.getSource()).getScene().getWindow();
             window.setScene(new Scene(FXMLLoader.load(getClass().getResource("../scenes/MainMenuScene.fxml"))));
             window.show();
@@ -69,6 +48,62 @@ public class PianoController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            loadChannel();
+        } catch (MidiUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public void playC1Button(ActionEvent actionEvent) {
+        channel.noteOn(60, 60);
+    }
+
+    public void playCSharp1Button(ActionEvent actionEvent) {
+        channel.noteOn(61, 60);
+    }
+
+    public void playD1Button(ActionEvent actionEvent) {
+        channel.noteOn(62, 60);
+    }
+
+    public void playDSharp1Button(ActionEvent actionEvent) {
+        channel.noteOn(63, 60);
+    }
+
+    public void playE1Button(ActionEvent actionEvent) {
+        channel.noteOn(64, 60);
+    }
+
+    public void playF1Button(ActionEvent actionEvent) {
+        channel.noteOn(65, 60);
+    }
+
+    public void playFSharp1Button(ActionEvent actionEvent) {
+        channel.noteOn(66, 60);
+    }
+
+    public void playG1Button(ActionEvent actionEvent) {
+        channel.noteOn(67, 60);
+    }
+
+    public void playGSharp1Button(ActionEvent actionEvent) {
+        channel.noteOn(68, 60);
+    }
+
+    public void playA1Button(ActionEvent actionEvent) {
+        channel.noteOn(69, 60);
+    }
+
+    public void playASharp1Button(ActionEvent actionEvent) {
+        channel.noteOn(70, 60);
+    }
+
+    public void playB1Button(ActionEvent actionEvent) {
+        channel.noteOn(71, 60);
+    }
+
+    public void playC2Button(ActionEvent actionEvent) {
+        channel.noteOn(72, 60);
     }
 }
